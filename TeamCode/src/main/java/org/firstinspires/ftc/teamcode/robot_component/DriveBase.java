b1d723af76d6fed42960db5b61756ac0bb75a8bf
ManyiZ <zhaomanyi2005@gmail.com>
2022-04-20 00:13:24 -0700

    Set up the controller class and need to test the drive powers

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot_component/DriveBase.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot_component/DriveBase.java
@@ -21,6 +21,18 @@ public class DriveBase {
         leftRearDrive = hardwareMap.get(DcMotor.class, "leftRearDrive");
         rightRearDrive = hardwareMap.get(DcMotor.class, "rightRearDrive");
 
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        leftFrontDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rightRearDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+
         this.telemetry = telemetry;
+    }
+
+    public void calculateDrivePower(double x, double y, double r){
+        leftFrontDrive.setPower(r + x + y);
+        leftRearDrive.setPower(r - x + y);
+        rightFrontDrive.setPower(r - x - y);
+        rightRearDrive.setPower(r + x - y);
     }
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Tele1.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/Tele1.java
@@ -3,17 +3,26 @@ package org.firstinspires.ftc.teamcode.teleop;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
+import org.firstinspires.ftc.teamcode.R;
+import org.firstinspires.ftc.teamcode.robot_component.Robot;
+import org.firstinspires.ftc.teamcode.robot_components.input.Controller;
+
 @TeleOp
 //Want to try using Opmode instead of LinearOp since I heard this is better for TeleOp
 public class Tele1 extends OpMode {
 
+    Robot robot;
+    Controller gp1;
+
     @Override
     public void init() {
-
+        robot = new Robot(hardwareMap, telemetry);
+        gp1 = new Controller(gamepad1);
     }
 
     @Override
     public void loop() {
-
+        robot.calculateDrivePower(gp1.left_stick_x, gp1.left_stick_y, gp1.right_stick_x);
+        telemetry.update();
     }
 }
